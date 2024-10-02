300e3088fdb6d902223e205b0ed122306fb861cc
sanatan01 <sanatan0660@gmail.com>
2024-10-02 10:51:25 +0000

    Added Least Connections

--- src/main.go
+++ src/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"sync"
 )
 
 func handleErr(err error){
@@ -19,12 +20,17 @@ type Server interface{
 	IsAlive() bool
 	Address() string
 	Serve( res http.ResponseWriter, req *http.Request)
+	GetConnections() int
+	IncrConnections()
+	DecrConnections()
 }
 
 
 type simpleServer struct{
 	address string
 	proxy *httputil.ReverseProxy
+	activeConnections int
+	mutex sync.Mutex
 }
 
 func newsimpleServer(address string)  *simpleServer{
@@ -49,6 +55,23 @@ func (s *simpleServer)Serve(res http.ResponseWriter,req *http.Request){
 	s.proxy.ServeHTTP(res,req)
 }
 
+func (s *simpleServer)GetConnections() int{
+	return s.activeConnections
+}
+
+func (s *simpleServer)IncrConnections(){
+	s.mutex.Lock()
+    defer s.mutex.Unlock()
+    s.activeConnections++
+	fmt.Printf("Server %s incremented connections: %d\n", s.address, s.activeConnections)
+}
+
+func (s *simpleServer)DecrConnections(){
+    s.mutex.Lock()
+    defer s.mutex.Unlock()
+    s.activeConnections--
+	fmt.Printf("Server %s decremented connections: %d\n", s.address, s.activeConnections)
+}
 
 
 type LoadBalancer struct{
@@ -65,8 +88,7 @@ func newLoadBalancer(port string,servers []Server) *LoadBalancer{
 	}
 }
 
-func (lb *LoadBalancer)nextAvailableServer() Server{
-
+func (lb *LoadBalancer)getNextRoundRobin() Server{
 	//  go to the next till you get a alive one
 	server:= lb.servers[lb.roundRobinCount%len(lb.servers)]
 	for !server.IsAlive(){
@@ -77,18 +99,47 @@ func (lb *LoadBalancer)nextAvailableServer() Server{
 	return server
 }
 
-func (lb *LoadBalancer) serveProxy(req *http.Request,res http.ResponseWriter){
-	targetServer:=lb.nextAvailableServer()
+func (lb *LoadBalancer)getNextLeastConnection() Server{
+	minConnections := lb.servers[0].GetConnections()
+	minServer := lb.servers[0]
+
+	for _, server := range lb.servers {
+		if server.GetConnections() < minConnections {
+			minConnections = server.GetConnections()
+			minServer = server
+		}
+	}
+
+	minServer.IncrConnections()
+	return minServer
+}
+
+func (lb *LoadBalancer) serveProxyRoundRobin(req *http.Request,res http.ResponseWriter){
+	targetServer:=lb.getNextRoundRobin()
+
+	fmt.Printf("forwarding request to the server with address : %v\n",targetServer.Address())
+	targetServer.Serve(res,req)
+}
+
+func (lb *LoadBalancer) serveProxyLeastConnection(req *http.Request,res http.ResponseWriter){
+	targetServer := lb.getNextLeastConnection()
+
+	defer func() {
+		targetServer.DecrConnections()
+	}()
 
 	fmt.Printf("forwarding request to the server with address : %v\n",targetServer.Address())
 	targetServer.Serve(res,req)
 }
 
 
+
 func main(){
 
 	// make some simple servers using some existing urls and run it
 	servers := []Server{
+		newsimpleServer("https://postman-echo.com/delay/5"),
+		newsimpleServer("https://postman-echo.com/delay/10"),
 		newsimpleServer("https://www.facebook.com"),
 		newsimpleServer("https://www.bing.com"),
 		newsimpleServer("https://www.duckduckgo.com"),
@@ -96,7 +147,7 @@ func main(){
 
 	lb := newLoadBalancer("8000", servers)
 	handleRedirect := func(res http.ResponseWriter, req *http.Request) {
-		lb.serveProxy(req, res)
+		lb.serveProxyLeastConnection(req, res)
 	}
 
 	// register a proxy handler to handle all requests
